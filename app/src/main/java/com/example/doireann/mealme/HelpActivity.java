e0fc09369824f2a9e491071dc3094fb15573bc05
hublaw <ldoireann@gmail.com>
2018-04-12 10:30:55 -0400

    [Finishing] Add help text

--- app/src/main/java/com/example/doireann/mealme/HelpActivity.java
+++ app/src/main/java/com/example/doireann/mealme/HelpActivity.java
@@ -5,14 +5,23 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 public class HelpActivity extends AppCompatActivity {
-    TextView hSearch, hSuggest;
+    TextView hSuggest1, hSuggest2, hSuggest3, hSuggest4, hSuggest5;
+    TextView hSearch1, hSearch2, hSearch3, hSearch4, hSearch5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
 
-        hSearch = findViewById(R.id.id_help_search);
-        hSuggest = findViewById(R.id.id_help_suggest);
+        hSuggest1 = findViewById(R.id.id_help_suggest1);
+        hSuggest2 = findViewById(R.id.id_help_suggest2);
+        hSuggest3 = findViewById(R.id.id_help_suggest3);
+        hSuggest4 = findViewById(R.id.id_help_suggest4);
+        hSuggest5 = findViewById(R.id.id_help_suggest5);
+        hSearch1 = findViewById(R.id.id_help_search1);
+        hSearch2 = findViewById(R.id.id_help_search2);
+        hSearch3 = findViewById(R.id.id_help_search3);
+        hSearch4 = findViewById(R.id.id_help_search4);
+        hSearch5 = findViewById(R.id.id_help_search5);
     }
 }
